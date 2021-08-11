136b1871de11ab0029ba4464338e148610f5d3c7
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-11 13:48:46 +0200

    Ex3 - done - second solution (with Array)

--- exercise3/main.js
+++ exercise3/main.js
@@ -1,11 +1,18 @@
+const toDoList = [];
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-const tasksNumber = document.querySelector("h1 span");
-const taskList = document.querySelector("ul");
+
 const tasks = document.getElementsByClassName("task");
+const taskList = document.querySelector("ul");
+const tasksNumber = document.querySelector("h1 span");
 
 const removeTask = (e) => {
-  e.target.parentNode.remove();
+  const index = e.target.parentNode.dataset.key;
+  toDoList.splice(index, 1);
+
+  renderToDoList();
+
   tasksNumber.textContent = tasks.length;
 };
 
@@ -18,10 +25,23 @@ const addTask = (e) => {
   const task = document.createElement("li");
   task.className = "task";
   task.innerHTML = `${taskTitle} <button>x</button>`;
-  taskList.appendChild(task);
+  toDoList.push(task);
+
+  renderToDoList();
+
   input.value = "";
   tasksNumber.textContent = tasks.length;
   task.querySelector("button").addEventListener("click", removeTask);
+
+  console.log(toDoList);
+};
+
+const renderToDoList = () => {
+  taskList.textContent = "";
+  toDoList.forEach((toDoElement, key) => {
+    toDoElement.dataset.key = key;
+    taskList.appendChild(toDoElement);
+  });
 };
 
 form.addEventListener("submit", addTask);
